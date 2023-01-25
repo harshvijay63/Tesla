7d7a63102d14b4b22d6a9b824e2a595d5e998fb1
harshvijay <72139720+harshvijay63@users.noreply.github.com>
2023-01-25 16:45:01 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA36S8bWgIwIUB7V8XJP643nKzDIGN6fhI',
+  apiKey: 'AIzaSyI',
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: "G-FWS1ZCRH65"
+  measurementId: "G-5"
 };
 
 // Initialize Firebase
